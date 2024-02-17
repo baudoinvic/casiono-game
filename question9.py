1ebefa123033deca24584b86f1d8ac3cd83c74f8
baudoin <baudoinvicbolingo@gmail.com>
2024-02-17 13:15:52 +0200

    solving error

--- question9.py
+++ question9.py
@@ -1,19 +1,19 @@
-# class Human:
-#     def __init__(self, coins):
-#         self.coins = coins
+class Human:
+    def __init__(self, coins):
+        self.coins = coins
 
-#     def bet(self):
-#         bet_coin = int(input("How many coins do you bet?: (1-99) "))
-#         if 1 <= bet_coin <= 99:
-#             print(bet_coin)
-#             self.coins -= bet_coin
-#             print("MY:", self.coins)
-#         else:
-#             print("Invalid bet amount. Please enter a number between 1 and 99.")
+    def bet(self):
+        bet_coin = int(input("How many coins do you bet?: (1-99) "))
+        if 1 <= bet_coin <= 99:
+            print(bet_coin)
+            self.coins -= bet_coin
+            print("MY:", self.coins)
+        else:
+            print("Invalid bet amount. Please enter a number between 1 and 99.")
 
-# # Debugging the play() function
-# def play():
-#     human_player = Human(500)
-#     human_player.bet()
+# Debugging the play() function
+def play():
+    human_player = Human(500)
+    human_player.bet()
 
-# play()
+play()
