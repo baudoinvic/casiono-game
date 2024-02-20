90ac48dfde80bddc5d45b7764cda0b9ad6ba8c81
baudoin <baudoinvicbolingo@gmail.com>
2024-02-20 11:00:15 +0200

    still developing

--- question11.py
+++ question11.py
@@ -1,27 +1,27 @@
-class Player:
-    def __init__(self, name, coin):
-        self.name = name
-        self.coin = coin
+# class Player:
+#     def __init__(self, name, coin):
+#         self.name = name
+#         self.coin = coin
 
-class Human(Player):
-    pass
+# class Human(Player):
+#     pass
 
-class Computer(Player):
-    pass
+# class Computer(Player):
+#     pass
 
-players = []
+# players = []
 
-def create_players():
-    global players
-    players.append(Human('MY', 500))
-    players.append(Computer('C1', 500))
-    players.append(Computer('C2', 500))
-    players.append(Computer('C3', 500))
+# def create_players():
+#     global players
+#     players.append(Human('MY', 500))
+#     players.append(Computer('C1', 500))
+#     players.append(Computer('C2', 500))
+#     players.append(Computer('C3', 500))
 
-def play():
-    print('Debug: play()')
-    for player in players:
-        print(f"{player.name}: {player.coin}")
+# def play():
+#     print('Debug: play()')
+#     for player in players:
+#         print(f"{player.name}: {player.coin}")
 
-create_players()
-play()
+# create_players()
+# play()
